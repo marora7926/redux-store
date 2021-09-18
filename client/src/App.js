6ca7f2c6990316b568cf5674b7f26c35cdc35649
Mohit Arora <mohit.arora@sydney.edu.au>
2021-09-18 12:04:02 +1000

    fixed bug on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import Nav from './components/Nav';
 // import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
-import { ReduxStoreProvider } from './utils/ReduxStoreProvider';
+import ReduxStoreProvider from './utils/ReduxStoreProvider';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
